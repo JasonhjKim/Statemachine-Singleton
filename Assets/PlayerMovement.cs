f33ce1cf9327b3cd41abb7f75b7af9da577daa44
Hyun Jun Kim <jason@Hyuns-MacBook-Pro.local>
2017-08-03 13:25:42 -0700

    cleaned up Player movement script

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,21 +8,42 @@ public class PlayerMovement : MonoBehaviour {
 	[Header("Player Variables")]
 	public float playerMovementSpeed;
 	public float playerAttackSpeed;
+	public float jumpHeight = 4;
+	public float timeToJumpApex = 0.4f;
+	public float dashSpeed = 2000;
+	public float moveSpeed = 6;
 
-	//private Variables
-	private Vector2 input;
-	private Vector2 velocity;
+	float acclerationTimeAirborne = 0.01f;
+	float acclerationTimeGrounded = 0.1f;
+	int keyCounter = 0;
+	float impulseDistance = 5;
+	float gravity;
+	float jumpVelocity;
+	Vector3 velocity;
+	float velocityXSmoothing;
+
+	//private Variable
 
 	void Start () {
 		controller = GetComponent<PlayerController2D> ();
+		gravity = -((2 * jumpHeight) / Mathf.Pow (timeToJumpApex, 2));
+		jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+		print ("Gravity: " + gravity + " Jump Velocity: " + jumpVelocity);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		if (controller.collisions.above || controller.collisions.below) {
+			velocity.y = 0;
+		}
+		if (Input.GetKeyDown (KeyCode.Space) && controller.collisions.below) {
+			velocity.y = jumpVelocity;
+		}
 
-		velocity.x += input.x * playerMovementSpeed * Time.deltaTime;
-
+		float targetVelocityX = input.x * moveSpeed;
+		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?acclerationTimeGrounded:acclerationTimeAirborne);
+		velocity.y += gravity * Time.deltaTime;
 		controller.Move (velocity * Time.deltaTime);
 	}
 }
