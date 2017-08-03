6fc21c38382687b717cba98e39f62c162d22398e
JasonDev <jasonhj62kim@gmail.com>
2017-08-03 13:18:53 -0700

    added new State

---
+++ Assets/AttackState.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StateStuff;
+
+public class AttackState : State<AI>{
+	public static AttackState _instance;
+
+	private AttackState() {
+		if (_instance != null) {
+			return;
+		}
+		_instance = this;
+	}
+
+	public static AttackState Instance {
+		get {
+			if (_instance == null) {
+				new AttackState ();
+			}
+			return _instance;
+		}
+	}
+
+	public override void EnterState (AI _owner) {
+		_owner.gameObject.GetComponent<Renderer> ().material.color = Color.red;
+		Debug.Log ("Entering Attack State");
+	}
+
+	public override void ExitState (AI _owner) {
+		Debug.Log ("Exiting Attack State");
+	}
+
+	public override void UpdateState(AI _owner) {
+		Debug.Log (DistanceFromTarget (_owner));
+		if (DistanceFromTarget (_owner) > _owner.attackRange) {
+			_owner.stateMachine.ChangeState (ChaseState.Instance);
+		}
+		Debug.Log("Currently Attacking");
+	}
+
+	private float DistanceFromTarget(AI _owner) {
+		return Vector2.Distance (_owner.target.transform.position, _owner.transform.position);
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	PlayerController2D controller;
+
+	[Header("Player Variables")]
+	public float playerMovementSpeed;
+	public float playerAttackSpeed;
+
+	//private Variables
+	private Vector2 input;
+	private Vector2 velocity;
+
+	void Start () {
+		controller = GetComponent<PlayerController2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+		velocity.x += input.x * playerMovementSpeed * Time.deltaTime;
+
+		controller.Move (velocity * Time.deltaTime);
+	}
+}
